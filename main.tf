7f9be07db898f6eda2ee3f46e85a6b76737080da
Sergiy <23329191+stsyg@users.noreply.github.com>
2022-09-28 22:49:43 -0600

    updated variables #3

--- main.tf
+++ main.tf
@@ -17,20 +17,20 @@ resource "azurerm_network_security_group" "infransg" {
 
 # Create vNet and Subnets
 resource "azurerm_virtual_network" "infravnet" {
-  name                = var.name_vnet
+  name                = var.virtual_network.name_vnet
   location            = azurerm_resource_group.infrarg.location
   resource_group_name = azurerm_resource_group.infrarg.name
-  address_space       = var.address_space
+  address_space       = var.virtual_network.address_space
 
   subnet {
-    name           = var.name_subnet1
-    address_prefix = var.address_prefix1
+    name           = var.virtual_network.name_subnet1
+    address_prefix = var.virtual_network.address_prefix1
     security_group = azurerm_network_security_group.infransg.id
   }
 
   subnet {
-    name           = var.name_subnet2
-    address_prefix = var.address_prefix2
+    name           = var.virtual_network.name_subnet2
+    address_prefix = var.virtual_network.address_prefix2
     security_group = azurerm_network_security_group.infransg.id
   }
 
