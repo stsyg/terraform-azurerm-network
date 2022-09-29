b7906082764a508d6f87a198f4088a52aeb5d89d
Sergiy <23329191+stsyg@users.noreply.github.com>
2022-09-28 22:54:44 -0600

    updated address space formating #2

--- main.tf
+++ main.tf
@@ -20,17 +20,17 @@ resource "azurerm_virtual_network" "infravnet" {
   name                = var.virtual_network.name_vnet
   location            = azurerm_resource_group.infrarg.location
   resource_group_name = azurerm_resource_group.infrarg.name
-  address_space       = var.virtual_network.address_space
+  address_space       = [var.virtual_network.address_space]
 
   subnet {
     name           = var.virtual_network.name_subnet1
-    address_prefix = var.virtual_network.address_prefix1
+    address_prefix = [var.virtual_network.address_prefix1]
     security_group = azurerm_network_security_group.infransg.id
   }
 
   subnet {
     name           = var.virtual_network.name_subnet2
-    address_prefix = var.virtual_network.address_prefix2
+    address_prefix = [var.virtual_network.address_prefix2]
     security_group = azurerm_network_security_group.infransg.id
   }
 
